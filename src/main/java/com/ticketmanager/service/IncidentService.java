0339dcc7ef327914cb17c2a71bd2c9d680828527
Kelvin <kelvintenor987@gmail.com>
2026-03-20 22:19:08 -0300

    feat: add IncidentService with registrate, showAll, showById and remove

---
+++ src/main/java/com/ticketmanager/service/IncidentService.java
@@ -0,0 +1,33 @@
+package com.ticketmanager.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.ticketmanager.model.Incident;
+import com.ticketmanager.repository.IncidentRepository;
+
+public class IncidentService {
+
+    private IncidentRepository repository;
+
+    public IncidentService() {
+        this.repository = new IncidentRepository();
+    }
+
+    public void registrate(Incident incident){
+        repository.save(incident);
+    }
+
+    public List<Incident> showAll() {
+        return repository.findAll();
+    }
+
+    public Optional<Incident> showById(int id){
+        return repository.findById(id);
+    }
+
+    public void remove(int id){
+        repository.delete(id);
+    }
+
+}
